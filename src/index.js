535e526776fe63152c1d27b8f5a7a423ba725d85
谢天宸 <xietianchen@shimo.im>
2020-12-03 23:08:28 +0800

    feat: hw

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Game } from "./reactDemos/game/game";
+import App from "./reactDemos/example/App";
+//import { Game } from "./reactDemos/game/game";
 
 
-ReactDOM.render(<Game />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
