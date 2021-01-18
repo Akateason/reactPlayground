a07c02069bb416266e760db5fd2385b2e3aca73c
谢天宸 <xietianchen@shimo.im>
2021-01-18 09:55:22 +0800

    feature: redux todolist 走通流程 1搜索2增加3删除

--- src/reactDemos/reduxDemo/TodoList.js
+++ src/reactDemos/reduxDemo/TodoList.js
@@ -7,6 +7,10 @@ class TodoList extends Component {
   constructor(props) {
     super(props);
     this.state = store.getState();  // 从redux拿state
+    this.changeInputValue = this.changeInputValue.bind(this);
+    this.storeChange = this.storeChange.bind(this);
+    this.clickBtn = this.clickBtn.bind(this);    
+    store.subscribe(this.storeChange) //订阅
   }
 
   render() { 
@@ -16,19 +20,54 @@ class TodoList extends Component {
           <Input 
             placeholder={this.state.inputValue}
             style={{width:'250px', marginRight:'10px'}} 
+            onChange={this.changeInputValue}
+            value={this.state.inputValue}
           />
-          <Button type="primary">增加</Button>
+          <Button 
+            type="primary"
+            onClick={this.clickBtn}
+            >增加</Button>
         </div>
 
         <div style={{margin:'10px',width:'300px'}}>
           <List
             bordered
             dataSource={this.state.list}
-            renderItem={item=>(<List.Item>{item}</List.Item>)}
+            renderItem={
+              (item,index)=>(
+              <List.Item onClick={this.deleteItem.bind(this,index)}>
+                {item}
+              </List.Item>)}
           />
         </div>
       </div> 
     );
+  }
+
+  changeInputValue(e){       
+    //console.log(e.target.value);
+    const action = {
+      type: 'changeInputValue',
+      value: e.target.value
+    }
+    store.dispatch(action);
+  }
+
+  storeChange(){
+    this.setState(store.getState());
+  }
+
+  clickBtn(){
+    const action = {type:'addItem'};
+    store.dispatch(action);
+  }
+
+  deleteItem(index){
+    const action = {
+      type:'deleteItem',
+      index
+    };
+    store.dispatch(action)
   }
 }
  
--- src/reactDemos/reduxDemo/store/reducer.js
+++ src/reactDemos/reduxDemo/store/reducer.js
@@ -10,5 +10,26 @@ const defaultState = {
 }
 
 export default (state = defaultState, action)=>{
+  console.log(state,action);//结合log看懂action传过来了
+  //Reducer里只能接收State, 不能改变State.
+  if (action.type==="changeInputValue") {
+    let newState = JSON.parse(JSON.stringify(state));//深拷贝
+    newState.inputValue = action.value;
+    return newState;//处理完业务逻辑返回仓库.
+  }
+
+  if (action.type === 'addItem') {
+    let newState = JSON.parse(JSON.stringify(state));//深拷贝
+    newState.list.push(newState.inputValue);
+    newState.inputValue = '';
+    return newState;
+  }
+
+  if (action.type === 'deleteItem') {
+    let newState = JSON.parse(JSON.stringify(state));//深拷贝
+    newState.list.splice(action.index,1);    
+    return newState;
+  }
+
   return state
 }
