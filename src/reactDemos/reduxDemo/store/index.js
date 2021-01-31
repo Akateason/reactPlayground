6a02a2a4dc2e331aa443763f7fb0c13c1a2691aa
谢天宸 <xietianchen@shimo.im>
2021-01-31 22:08:05 +0800

    chore

--- src/reactDemos/reduxDemo/store/index.js
+++ src/reactDemos/reduxDemo/store/index.js
@@ -1,7 +1,9 @@
 import {createStore}  from 'redux'
 import reducer from './reducer';
 
+//store必须是唯一的
 //store相当于 仓库(图书馆)
+
 const store = createStore(
   reducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
--- src/reactDemos/reduxDemo/store/reducer.js
+++ src/reactDemos/reduxDemo/store/reducer.js
@@ -1,4 +1,6 @@
 // reducer相当于图书管理员
+// Reducer里只能接收State, 不能改变State.
+// reducer必须是一个纯函数, 变化只依赖于参数(state,action). 不能返回和参数无关的结果.
 
 import { ADD_ITEM, CHANGE_INPUT, DELETE_ITEM } from "./actionTypes";
 
@@ -12,8 +14,8 @@ const defaultState = {
 }
 
 export default (state = defaultState, action)=>{
-  console.log(state,action);//结合log看懂action传过来了
-  //Reducer里只能接收State, 不能改变State.
+  console.log(state,action); // 结合log看懂action传过来了
+  // Reducer里只能接收State, 不能改变State.
   if (action.type === CHANGE_INPUT) {
     let newState = JSON.parse(JSON.stringify(state));//深拷贝
     newState.inputValue = action.value;
