11a183acc08872af8678cd066b98a8bfe8e16842
谢天宸 <xietianchen@shimo.im>
2021-02-18 09:15:26 +0800

    chore: toDolistUI改成无状态组件

--- src/reactDemos/reduxDemo/TodoListUI.js
+++ src/reactDemos/reduxDemo/TodoListUI.js
@@ -1,45 +1,38 @@
-import React, { Component } from 'react';
+import React from 'react';
 import 'antd/dist/antd.css'
 import { Input, Button, List } from 'antd';
 
-class TodoListUI extends Component {
+// TodoListUI 抽离出UI部分，把class改成func（无状态组件,没有state，没有生命周期，性能会高一些,没有任何业务逻辑，只有UI）
+const TodoListUI = (props)=>{
+  return ( 
+    <div style={{margin:'10'}}>
+    <div>
+      <Input 
+        placeholder={props.inputValue}
+        style={{width:'250px', marginRight:'10px'}} 
+        onChange={props.changeInputValue}
+        value={props.inputValue}
+      />
+      <Button 
+        type="primary"
+        onClick={props.clickBtn}
+        >增加</Button>
+    </div>
 
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {  }
-  // }
-
-  render() { 
-    return ( 
-      <div style={{margin:'10'}}>
-      <div>
-        <Input 
-          placeholder={this.props.inputValue}
-          style={{width:'250px', marginRight:'10px'}} 
-          onChange={this.props.changeInputValue}
-          value={this.props.inputValue}
-        />
-        <Button 
-          type="primary"
-          onClick={this.props.clickBtn}
-          >增加</Button>
-      </div>
-
-      <div style={{margin:'10px',width:'300px'}}>
-        <List
-          bordered
-          dataSource={this.props.list}
-          renderItem={
-            (item,index)=>(
-            <List.Item onClick={()=>{this.props.deleteItem(index)}}>
-              {item}
-            </List.Item>)
-          }   
-        />
-      </div>
-    </div> 
-      );
-  }
+    <div style={{margin:'10px',width:'300px'}}>
+      <List
+        bordered
+        dataSource={props.list}
+        renderItem={
+          (item,index)=>(
+          <List.Item onClick={()=>{props.deleteItem(index)}}>
+            {item}
+          </List.Item>)
+        }   
+      />
+    </div>
+  </div> 
+    );
 }
- 
+
 export default TodoListUI;
